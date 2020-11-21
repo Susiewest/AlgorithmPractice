c45d5ccab95a34d9d013d93db0bcb03dd959cb02
Susiewest <52514773+Susiewest@users.noreply.github.com>
2020-11-21 18:12:28 +0800

    Update 300. Longest Increasing Subsequence.py

--- Dynamic Programming/300. Longest Increasing Subsequence.py
+++ Dynamic Programming/300. Longest Increasing Subsequence.py
@@ -23,4 +23,55 @@ class Solution:
 执行用时：3752 ms, 在所有 Python3 提交中击败了5.03%的用户
 内存消耗：13.8 MB, 在所有 Python3 提交中击败了5.36%的用户
 
+那么将dp（为了区分 这里叫tail） 将tail[j]定义为保存到nums[i]为止的最大升序子数组的末尾数，j是最大升序子数组的长度，最后tail的长度就是想要的解
+也就是，遇到一个nums[i]，和tail[-1]比较，如果大，就跟在后面，如果小，就去更新整个tail里第一个比nums[i]大的数，为了保存同样长度，末尾数最小的子数组！
 
+以下的写法是错误的！测试用例为[4,10,4,3,8,9] 的时候，遇到第二个4，会去tail=[4,10]里先找到4，发现4不满足大于nums[i]会去右边找到10，然后替换掉10，这样不对
+所以27行说的找到第一个大于nums[i]不对，应该找到第一个大于等于nums[i]的，那么等号的处理方式应该和大于绑定，不应该和小于绑定，如果和小于绑定了，left会去找小于等于nums[i]的下一个！
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        tail = [nums[0]]
+        for i in range(1,len(nums)):
+            if nums[i]>tail[-1]:
+                tail.append(nums[i])
+                continue
+            if nums[i]==tail[-1]:
+                continue
+            left, right = 0, len(tail)-1
+            while(left<right):
+                #mid = left+(right-left)//2
+                mid = (left+right)>>1
+                if tail[mid]>nums[i]:
+                    #right = mid-1
+                    right = mid
+                else:
+                    left = mid+1
+            tail[left] = nums[i] #left最后会指向第一个大于nums[i]的
+        return len(tail)
+
+以下为正确写法
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        tail = [nums[0]]
+        for i in range(1,len(nums)):
+            if nums[i]>tail[-1]:
+                tail.append(nums[i])
+                continue
+            if nums[i]==tail[-1]:
+                continue
+            left, right = 0, len(tail)-1
+            while(left<right):
+                #mid = left+(right-left)//2
+                mid = (left+right)>>1
+                if tail[mid]<nums[i]:
+                    #right = mid-1
+                    left = mid+1
+                else:
+                    right = mid
+            tail[left] = nums[i] #left最后会指向第一个大于nums[i]的
+        return len(tail)
+    
+执行用时：72 ms, 在所有 Python3 提交中击败了75.22%的用户
+内存消耗：13.6 MB, 在所有 Python3 提交中击败了26.91%的用户
