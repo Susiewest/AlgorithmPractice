3d5cee4a2a088d8dc35d0604eb5eb3bafa0068b8
Susiewest <52514773+Susiewest@users.noreply.github.com>
2020-12-10 11:43:19 +0800

    Update 17. Letter Combinations of a Phone Number.py

--- Backtracking/17. Letter Combinations of a Phone Number.py
+++ Backtracking/17. Letter Combinations of a Phone Number.py
@@ -25,3 +25,21 @@ class Solution:
 44 ms, 在所有 Python3 提交中击败了43.88%的用户
 内存消耗：
 13.4 MB, 在所有 Python3 提交中击败了60.44%的用户'''
+不递归的做法！
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        combination=[]
+        if not digits:
+            return combination
+        combination=['']
+        tmp=''
+        chart=['abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
+        for i in digits:
+            for _ in range(len(combination)): #对每个新i 当前comb里面的所有都是上层的节点
+                tmp=combination.pop(0)
+                for j in chart[ord(i)-50]: #ascii码-48转为数字，再-2转为从0开始
+                    combination.append(tmp+j)
+        return combination
+
+执行用时：52 ms, 在所有 Python3 提交中击败了6.45%的用户
+内存消耗：13.5 MB, 在所有 Python3 提交中击败了30.91%的用户
