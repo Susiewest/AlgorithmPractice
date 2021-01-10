a6365050f5ce8653d77e5d6f33b29cb94fb130f0
Susiewest <52514773+Susiewest@users.noreply.github.com>
2021-01-10 15:34:58 +0800

    Create 剑指 Offer 52. 两个链表的第一个公共节点.py

---
+++ LinkList/剑指 Offer 52. 两个链表的第一个公共节点.py
@@ -0,0 +1,25 @@
+要把none==none的情况保证能够执行while 
+否则若无交点会超时
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        p1, p2 = headA, headB
+        while p1 != p2:
+            if not p1:
+                p1 = headB
+            else:
+                p1 = p1.next
+            if not p2:
+                p2 = headA
+            else:
+                p2 = p2.next
+        return p1
+        
+执行用时：164 ms, 在所有 Python3 提交中击败了74.89%的用户
+内存消耗：29.4 MB, 在所有 Python3 提交中击败了25.74%的用户
