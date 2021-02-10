60685520f6a455851506a6a4c3c8f45474c3255f
Susiewest <52514773+Susiewest@users.noreply.github.com>
2021-02-10 23:22:49 +0800

    Update 450. Delete Node in a BST.py

--- Tree/450. Delete Node in a BST.py
+++ Tree/450. Delete Node in a BST.py
@@ -55,3 +55,50 @@ c.right = g
 sol = Solution()
 ans=sol.deleteNode(a,3)
 print(ans)
+
+其实没有完全明白上面为什么错，p = none这步成功的执行了，但却没有修改树本身 p.val却成功的修改了树本身
+
+如果说一层循环只能处理当前root，不能用while的写法利用p找到要删除的节点，然后处理完再返回root。
+那么就只处理root的办法是以下写法。
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def find_predecessor(self, root):
+        root = root.left
+        while root.right:
+            root = root.right
+        return root.val
+        
+    def find_successor(self, root):
+        root = root.right
+        while root.left:
+            root = root.left
+        return root.val
+
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root:
+            return None
+        if root.val>key:
+            root.left = self.deleteNode(root.left, key)。#1
+        elif root.val<key:
+            root.right = self.deleteNode(root.right, key)。#1
+        else:
+            if not (root.left or root.right):
+                root = None
+            elif root.left:
+                root.val = self.find_predecessor(root)
+                root.left = self.deleteNode(root.left, root.val)。 #2
+            else:
+                root.val = self.find_successor(root)
+                root.right = self.deleteNode(root.right, root.val)。#2
+        return root
+    
+    1/2处的root.left = 和root.right = 都不能省略
+执行用时：88 ms, 在所有 Python3 提交中击败了39.32%的用户
+内存消耗：19 MB, 在所有 Python3 提交中击败了13.55%的用户
+
+
