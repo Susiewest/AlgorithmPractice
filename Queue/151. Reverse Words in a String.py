c470f8d106df0248ba74cd709a70afa774fb9296
Susiewest <52514773+Susiewest@users.noreply.github.com>
2021-02-23 22:46:05 +0800

    Update 151. Reverse Words in a String.py

--- Queue/151. Reverse Words in a String.py
+++ Queue/151. Reverse Words in a String.py
@@ -8,10 +8,15 @@ class Solution:
 执行用时：64 ms, 在所有 Python3 提交中击败了6.00%的用户
 内存消耗：14.9 MB, 在所有 Python3 提交中击败了58.84%的用户
 
-双端队列法待补充
+双端队列法
+word保存当前单词，完整后保存到双端队列，从左端送入
+双端队列的appendleft优于list的insert
+有两个地方需要注意 1. word被加入result的判断条件，如果只在遇到空格时就加入，遇到连续的两个空格会保留一个空格，这样是不对的。因此在遇到空格并且word不为空时加入。
+2. 最后一个单词遇不到空格 无法正确加入result，因为结束前要再加入一次
+ps：仍然不是O(1)空间
 class Solution:
     def reverseWords(self, s: str) -> str:
-        start, end = 0, len(s)
+        start, end = 0, len(s)-1
         while s[start]==' ':
             start+=1
         while s[end]==' ':
@@ -21,7 +26,14 @@ class Solution:
         while(start<=end):
             if s[start]!=' ':
                 word.append(s[start])
-            elif s[start]==' ':
+            # "a good   example"-->"example good a"
+            elif s[start]==' ' and word:         #1
                 result.appendleft(''.join(word))
+                word = []
             start+=1
+        result.appendleft(''.join(word))      #2
         return ' '.join(result)
+   
+执行用时：56 ms, 在所有 Python3 提交中击败了13.48%的用户
+内存消耗：15.1 MB, 在所有 Python3 提交中击败了20.35%的用户
+
