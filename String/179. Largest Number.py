5fe3409fe8996ce7bfbca42fb0876329f9b5d006
Susiewest <52514773+Susiewest@users.noreply.github.com>
2021-04-12 23:27:09 +0800

    Update 179. Largest Number.py

--- String/179. Largest Number.py
+++ String/179. Largest Number.py
@@ -18,3 +18,35 @@ class Solution:
 
 执行用时：44 ms, 在所有 Python3 提交中击败了80.62%的用户
 内存消耗：13.6 MB, 在所有 Python3 提交中击败了7.16%的用户
+
+参考剑指45的做法
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        #map() 会根据提供的函数对指定序列做映射。
+        #第一个参数 function 以参数序列中的每一个元素调用 function 函数，返回包含每次 function 函数返回值的新列表。
+        #str_nums = map(str, nums)
+        def fastsort(left, right):
+            if left>=right:
+                return 
+            pivot = str_nums[left]
+            i, j = left, right
+            while i<j:
+                while i<j and str_nums[j]+pivot>=pivot+str_nums[j]:
+                    j -= 1
+                str_nums[i] = str_nums[j]
+                while i<j and str_nums[i]+pivot<=pivot+str_nums[i]:
+                    i += 1
+                str_nums[j] = str_nums[i]
+            str_nums[i] = pivot
+            fastsort(left, i-1)
+            fastsort(i+1, right)
+        str_nums = [str(i) for i in nums]
+        fastsort(0, len(nums)-1)
+        str_nums = str_nums[::-1]
+        result = ''.join(str_nums) 
+        return result if int(result) else '0'
+
+执行用时: 44 ms
+内存消耗: 15.1 MB
+
