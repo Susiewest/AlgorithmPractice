c4eb75820ad4f82631e63c984cc9a4295c409b1b
Susiewest <52514773+Susiewest@users.noreply.github.com>
2021-05-12 17:28:25 +0800

    Create 链表求和.py

---
+++ LinkList/链表求和.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        s1 = ''
+        s2 = ''
+        while l1:
+            s1 += str(l1.val)
+            l1 = l1.next
+        while l2:
+            s2 += str(l2.val)
+            l2 = l2.next
+        s1 = s1[::-1]
+        s2 = s2[::-1]
+        result = int(s1)+int(s2)
+        ListNode
