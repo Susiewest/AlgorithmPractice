d3441ffd06161262615951d16e63611b19726e7c
Susiewest <52514773+Susiewest@users.noreply.github.com>
2021-06-21 17:11:08 +0800

    Create 31. Next Permutation.py

---
+++ Array/31. Next Permutation.py
@@ -0,0 +1,30 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        less = len(nums)-2
+        # 从右往左找到第一个非升序的 如4，7，6，5，3 找到4
+        # less>=0 不写会越界，而且也无法正确执行less<0的情况（nums直接是最大）
+        while less>=0 and nums[less]>=nums[less+1]:  #**
+            less -= 1
+        # 若整个nums都是降序，那么已经是最大的排列了，直接反转变成最小的排列，跳过if里面的部分直接双指针反转
+        if less>=0:                                 #**
+            # 找到第一个比nums[less]大的
+            more = len(nums)-1
+            # 这里的=也很关键啊啊啊，一定要找比nums[less]大的，而非小或者一样大的
+            while nums[more]<=nums[less]:           #**
+                more -= 1
+            nums[less], nums[more] = nums[more], nums[less]
+            # 把less+1：的元素统统反转（即less+1到最后一个元素 都是降序的，全变成升序）
+        left, right = less+1, len(nums)-1
+        while left<right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+        return nums
+        
+        
+执行用时：44 ms, 在所有 Python3 提交中击败了42.03%的用户
+内存消耗：14.9 MB, 在所有 Python3 提交中击败了46.52%的用户
+
