9575acf80ca7226d70e4de5bb06dd60bd9fe798f
Susiewest <52514773+Susiewest@users.noreply.github.com>
2021-06-28 20:47:45 +0800

    Update 47. Permutations II.py

--- Backtracking/47. Permutations II.py
+++ Backtracking/47. Permutations II.py
@@ -1,3 +1,6 @@
+只有在新选择的元素，和上一个被释放的元素相同时，剪枝。
+只可能两个1都在/都不在，目前列表中只有一个，且和上一个选过的相同的话就要剪枝掉一种情况。
+
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         def backtrack(temp, nums, used):
@@ -7,7 +10,7 @@ class Solution:
             for i in range(len(nums)):
                 if used[i]==1:
                     continue
-                if i>0 and nums[i]==nums[i-1] and used[i-1]==0: #是判断另一个重复的用了没，没用，那就别选我，用了，再选我；先选前面的再选我，为了避免两个重复元素里，出现先选它再选我+先选我再选它当成不同的两个结果。
+                if i>0 and nums[i]==nums[i-1] and used[i-1]==0: #是判断另一个重复的用了没，没用（被撤消了选择），那就别选我，用了，再选我；先选前面的再选我，为了避免两个重复元素里，出现先选它再选我+先选我再选它当成不同的两个结果。
                 #如果判断 usedused[i]==0，那就continue的话，永远也没法达到len(temp)==len(nums)
                     continue                               
                 used[i] = 1
