30746ab7c3298a5453ec013a50048f134a13e772
Susiewest <52514773+Susiewest@users.noreply.github.com>
2021-07-05 20:36:58 +0800

    Update 17. Letter Combinations of a Phone Number.py

--- Backtracking/17. Letter Combinations of a Phone Number.py
+++ Backtracking/17. Letter Combinations of a Phone Number.py
@@ -43,3 +43,26 @@ class Solution:
 
 执行用时：52 ms, 在所有 Python3 提交中击败了6.45%的用户
 内存消耗：13.5 MB, 在所有 Python3 提交中击败了30.91%的用户
+
+#如果把自己定义的函数不以并列的形式def 而是包含的方式def 就不一定要return一个值
+#而且也不需要把其他用到的变量传递进去
+#妙呀
+
+本来觉得回溯法不需要设置depth，但写着写着发现不设置的话，无法知道走到digits的第几个位置了
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        self.combination=[]
+        if not digits:
+            return self.combination       
+        path=''
+        chart=['abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
+        def backtrack(path, depth):
+            if len(path)==len(digits):
+                self.combination.append(path)
+                return
+            for letter in chart[int(digits[depth])-2]:
+                backtrack(path+letter, depth+1)
+        backtrack('',0)
+        return self.combination
+执行用时：40 ms, 在所有 Python3 提交中击败了62.12%的用户
+内存消耗：15.1 MB, 在所有 Python3 提交中击败了9.78%的用户
