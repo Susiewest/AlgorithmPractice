d42739353b84143b858d898a1914f8bb41bfc27a
Susiewest <52514773+Susiewest@users.noreply.github.com>
2021-07-09 21:18:03 +0800

    Update 130. Surrounded Regions.py

--- Depth-first Search/130. Surrounded Regions.py
+++ Depth-first Search/130. Surrounded Regions.py
@@ -36,3 +36,44 @@ class Solution:
                     
 执行用时：60 ms, 在所有 Python3 提交中击败了43.96%的用户
 内存消耗：15 MB, 在所有 Python3 提交中击败了25.34%的用户
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        if not board:
+            return []
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+        def dfs(board, x, y, row, col):
+            board[x][y]='B'
+            for direction in directions:
+                new_x = x + direction[0]
+                new_y = y + direction[1]
+                if 0<=new_x<row and 0<=new_y<col:
+                    if board[new_x][new_y]=='O':
+                        dfs(board, new_x, new_y, row, col)
+        row = len(board)
+        col = len(board[0])
+        for i in range(row):
+            if board[i][0]=='O':
+                dfs(board, i, 0, row, col)
+            if board[i][col-1]=='O':
+                dfs(board, i, col-1, row, col)
+        for j in range(col):
+            if board[0][j]=='O':
+                dfs(board, 0, j, row, col)
+            if board[row-1][j]=='O':
+                dfs(board, row-1, j, row, col)
+        for i in range(row):
+            for j in range(col):
+                if board[i][j]=='O':
+                    board[i][j]='X'
+                if board[i][j]=='B':
+                    board[i][j]='O'
+        return board
+
+
+执行用时：40 ms, 在所有 Python3 提交中击败了99.11%的用户
+内存消耗：18.7 MB, 在所有 Python3 提交中击败了69.50%的用户
+
