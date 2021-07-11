8adc6cd010db79e9812215804a173910df5286ae
Susiewest <52514773+Susiewest@users.noreply.github.com>
2021-07-11 19:04:59 +0800

    Update 69. Sqrt(x).py

--- Math/69. Sqrt(x).py
+++ Math/69. Sqrt(x).py
@@ -20,3 +20,26 @@ class Solution:
 44 ms, 在所有 Python3 提交中击败了84.16%的用户
 内存消耗：
 13.6 MB, 在所有 Python3 提交中击败了6.06%的用户'''
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        # y = x^2 - c， c是待平方根的数（本题的x）
+        # 零点就是本题的解
+        # 求切线和x轴的交点作为下一个判断的点
+        # 初始化的点设为c是为了不要找到负轴的解
+        # y0’ = 2X0
+        # y - y0 = 2X0(X-X0)
+        # y=0, x = X0-y0/(2X0), y0=x0^2-c
+        # x = x0/2 +c/(2X0), 零点，作为下一轮的x0
+        if x==0:
+            return 0
+        x0 = x
+        pre = inf
+        while abs(pre-x0)>1e-7:
+            pre = x0
+            x0 = x0/2 + x/(2*x0)
+        return int(x0)
+
+
+执行用时：32 ms, 在所有 Python3 提交中击败了98.82%的用户
+内存消耗：15 MB, 在所有 Python3 提交中击败了5.25%的用户
