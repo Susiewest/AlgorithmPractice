91b769024927d8ea7a8d198fa4ff9dfe7aa05376
Susiewest <52514773+Susiewest@users.noreply.github.com>
2021-07-13 22:34:39 +0800

    Update 416. Partition Equal Subset Sum.py

--- Dynamic Programming/416. Partition Equal Subset Sum.py
+++ Dynamic Programming/416. Partition Equal Subset Sum.py
@@ -67,3 +67,26 @@ class Solution:
             last = current
             current = []
         return False
+
+    20210713
+    class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        # 如果最大的元素比target都要大，那分不了，如果和为奇数也分不了
+        n = len(nums)
+        if n<2:
+            return False
+        nums_sum = sum(nums)
+        if nums_sum%2==1 or nums_sum>>1<max(nums):
+            return False
+        target = nums_sum>>1
+        dp = [[False]*(target+1) for _ in range(n)]
+        for i in range(n):
+            dp[i][0] = True
+        dp[0][nums[0]] = True
+        for i in range(1, n):
+            for j in range(1, target+1):
+                if j>=nums[i]:
+                    dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i]]
+                else:
+                    dp[i][j] = dp[i-1][j]
+        return dp[-1][-1]
