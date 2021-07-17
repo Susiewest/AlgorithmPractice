a3c427a84146e8f9881d6d8e9256219e78ca5d89
Susiewest <52514773+Susiewest@users.noreply.github.com>
2021-07-17 22:37:08 +0800

    Update 1143. Longest Common Subsequence.py

--- Dynamic Programming/1143. Longest Common Subsequence.py
+++ Dynamic Programming/1143. Longest Common Subsequence.py
@@ -1,4 +1,6 @@
-感觉这个题其实跟编辑距离是一个思路
+感觉这个题其实跟编辑距离是一个思路，先判断当前位置的word1，word2相不相等，然后去基于二维数组里的左、上、左上 去取合适的值，
+不同的是 如果word1[i] == word2[j] 编辑距离是之前的dp，而在这个题，最长公共子序列是dp+1.
+
 0行0列代表着两个空字符串之间的对比，0行代表一个字符串为空，0列代表另一个字符串为空
 也就是dp[i][j]代表的是字符串0-(i-1)和0-(j-1)之间的最大公共子序列
 https://leetcode-cn.com/problems/longest-common-subsequence/solution/fu-xue-ming-zhu-er-wei-dong-tai-gui-hua-r5ez6/
