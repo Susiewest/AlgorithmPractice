fec14d52ea261f5f15612ae97db6b6b629a3161e
Susiewest <52514773+Susiewest@users.noreply.github.com>
2021-07-22 20:53:11 +0800

    Update 123. Best Time to Buy and Sell Stock III.py

--- Dynamic Programming/123. Best Time to Buy and Sell Stock III.py
+++ Dynamic Programming/123. Best Time to Buy and Sell Stock III.py
@@ -17,3 +17,14 @@ class Solution:
 执行用时：456 ms, 在所有 Python3 提交中击败了79.65%的用户
 内存消耗：25.1 MB, 在所有 Python3 提交中击败了59.48%的用户
 
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        buy1, buy2 = -prices[0], -prices[0]
+        sell1, sell2 = 0, 0
+        for i in range(n):
+            buy1 = max(buy1, -prices[i])
+            sell1 = max(sell1, buy1+prices[i])
+            buy2 = max(buy2, sell1-prices[i])
+            sell2 = max(sell2, buy2+prices[i])
+        return sell2
